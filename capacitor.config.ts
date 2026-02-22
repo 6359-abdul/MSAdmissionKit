53b5825ff9aebb4241030109f24d57df911c8dc5
6359-abdul <skmullasha6359@gmail.com>
2026-02-22 12:52:29 +0530

    Adding new branch

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mseaadmissionkit.app',
+  appName: 'MSEA-Admission Kit',
+  webDir: 'dist'
+};
+
+export default config;
